a9aacfbc764cfd2acbe799429dfbbfbd67b51b94
Lou Yunlong <lianer@live.cn>
2021-08-13 13:53:38 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,7 @@
+apt install git -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+npm i -g nrm
+nrm use taobao
+npm i -g pm2
